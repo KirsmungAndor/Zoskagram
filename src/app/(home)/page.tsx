bef767a78b107cd572fc86ee5a31a1aa1ccab69c
KirsmungAndor <DominikDrahuta@s.zochova.sk>
2024-09-26 08:40:14 +0200

    initial commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+// my-app/src/app/page.tsx
+
+import Typography from '@mui/material/Typography';
+
+export const metadata = { title: `Domov | Zoškagram`}
+
+export default function Home() {
+  return (
+    <Typography>Domovská stránka</Typography>
+  );
+}
